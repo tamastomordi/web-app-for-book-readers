da7d9047661edf1d1e3c889a26295b521a5c0a9f
Tamás Tömördi <tomordi.tamas@gmail.com>
2023-03-08 16:45:09 +0100

    Extend user model

--- rest-api/src/models/UserModel.py
+++ rest-api/src/models/UserModel.py
@@ -6,3 +6,8 @@ class UserModel(db.Model):
    username = db.Column('username_', db.String(50))
    email = db.Column('email_', db.String(255))
    password_hash = db.Column('password_hash_', db.Text)
+   sign_up_date = db.Column('sign_up_date_', db.Date)
+   birth_date = db.Column('birth_date_', db.Date)
+   user_img_path = db.Column('user_img_path_', db.Text)
+   location = db.Column('location_', db.String(255))
+   gender = db.Column('gender_', db.String(1))
