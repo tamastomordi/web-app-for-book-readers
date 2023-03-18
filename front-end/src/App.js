83e225de9ce8438012df808a5a9c83aa594aec37
Tamás Tömördi <tomordi.tamas@gmail.com>
2023-03-18 13:36:39 +0100

    Add nopage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './layouts/Header';
 import Home from './pages/Home';
+import NoPage from './pages/NoPage';
 import './styles/_reset.scss';
 import './styles/_global.scss';
 
@@ -10,7 +11,8 @@ const App = () => {
          <Header />
          <BrowserRouter>
             <Routes>
-               <Route path="/" element={<Home />}></Route>
+               <Route path="/" element={<Home />} />
+               <Route path="*" element={<NoPage />} />
             </Routes>
          </BrowserRouter>
       </div>
---
+++ front-end/src/pages/NoPage.js
@@ -0,0 +1,5 @@
+const NoPage = () => {
+   return <div className="NoPage">404: Page is not found!</div>;
+};
+
+export default NoPage;
