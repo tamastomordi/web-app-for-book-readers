1c233e84cc00e7b2fc38e82ab1ec1f7a0beaf6ae
Tamás Tömördi <tomordi.tamas@gmail.com>
2023-03-26 11:56:21 +0200

    Add welcome layout

--- front-end/src/layouts/Welcome.js
+++ front-end/src/layouts/Welcome.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 const Welcome = ({ username }) => {
    return (
       <div className="Welcome">
-         <h2>
-            Üdvözöllek újra itt, <Link>{username}</Link>!
-         </h2>
+         <h1>
+            Üdvözöllek újra itt, <Link to="/profile">{username}</Link>!
+         </h1>
       </div>
    );
 };
