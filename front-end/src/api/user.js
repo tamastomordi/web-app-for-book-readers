9a02155e832b02c610839c374ed56a472916db6d
Tamás Tömördi <tomordi.tamas@gmail.com>
2023-03-27 16:45:22 +0200

    Add get user api

---
+++ front-end/src/api/user.js
@@ -0,0 +1,8 @@
+import axios from './axios';
+
+export const getUser = async (userId) => {
+   const response = await axios.get('/get/user/' + userId).catch((error) => {
+      console.log(error);
+   });
+   return response.data;
+};
