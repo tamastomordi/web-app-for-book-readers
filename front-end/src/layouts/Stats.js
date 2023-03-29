9ecd0bc61fd91ab946eda8504f7b3543a205213b
Tamás Tömördi <tomordi.tamas@gmail.com>
2023-03-29 10:44:21 +0200

    Add stats

---
+++ front-end/src/layouts/Stats.js
@@ -0,0 +1,37 @@
+import '../styles/layouts/Stats.scss';
+
+const Stats = () => {
+   return (
+      <div className="Stats">
+         <h2>Statisztikák</h2>
+         <table>
+            <tr>
+               <th></th>
+               <th>A te olvasásaid</th>
+               <th>Barátaid olvasásai</th>
+               <th>Különbség</th>
+            </tr>
+            <tr>
+               <td>Ebben az évben:</td>
+               <td>10</td>
+               <td>15</td>
+               <td>+23%</td>
+            </tr>
+            <tr>
+               <td>Ebben a hónapban:</td>
+               <td>7</td>
+               <td>8</td>
+               <td>-10%</td>
+            </tr>
+            <tr>
+               <td>Ezen a héten:</td>
+               <td>2</td>
+               <td>5</td>
+               <td>+15%</td>
+            </tr>
+         </table>
+      </div>
+   );
+};
+
+export default Stats;
--- front-end/src/pages/Dashboard.js
+++ front-end/src/pages/Dashboard.js
@@ -1,16 +1,31 @@
-import { useRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { authState } from '../recoil/atoms/Auth';
 import Welcome from '../layouts/Welcome';
 import '../styles/pages/Dashboard.scss';
+import BookList from '../layouts/BookList';
+import { bookListState } from '../recoil/atoms/BookList';
+import { useEffect } from 'react';
+import { getBooks } from '../api/bookList';
+import { useRecoilState } from 'recoil';
+import Stats from '../layouts/Stats';
 
 const Dashboard = () => {
-   const [auth, setAuth] = useRecoilState(authState);
+   const auth = useRecoilValue(authState);
+   const [bookList, setBookList] = useRecoilState(bookListState);
+
+   useEffect(() => {
+      getBooks()
+         .then((data) => setBookList(data.books))
+         .catch((error) => console.log(error));
+   }, [setBookList]);
 
    return (
       <div className="Dashboard">
          <div className="container">
             <div className="card -wide">
                <Welcome username={auth.user.username} />
+               <Stats />
+               <BookList bookList={bookList} />
             </div>
          </div>
       </div>
